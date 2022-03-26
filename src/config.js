5d8c71afe3fb1cab5fd094328d485da828837f97
@x0mg <jeff14994@gmail.com>
2022-03-26 08:07:34 +0000

    Fix the .env problem

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // TESTNET CREDENTIALS
 // console.log(window.env);
 // put .env under the main folder not React folder
-const id = env.ID;
+const id = window.env.ID;
 // console.log(id)
-const pb_key = env.PB_KEY;
-const pvkey = env.PVKEY;
+const pb_key = window.env.PB_KEY;
+const pvkey = window.env.PVKEY;
 const operator = {
 	id: id,
 	pb_key: pb_key,
